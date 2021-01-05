ffe94e7714cbec5a12088ef67a17a51021572115
lauravikanis <lauravikanis@yahoo.de>
2021-01-05 18:43:36 +0100

    add global style to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import React from "react";
 import Page from "./pages/Page";
+import GlobalStyle from "./GlobalStyles";
 
 function App() {
   return (
-    <div className="App">
-      <Page />
-      <a href="/storybook">To Storybook</a>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <Page />
+        <a href="/storybook">To Storybook</a>
+      </div>
+    </>
   );
 }
 
