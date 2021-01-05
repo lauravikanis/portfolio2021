ac5f42ac1a8d83c13779a43ff22310d3c9843dc4
Laura <67901192+lauravikanis@users.noreply.github.com>
2021-01-05 18:34:02 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import Page from "./pages/Page";
+
+function App() {
+  return (
+    <div className="App">
+      <Page />
+      <a href="/storybook">To Storybook</a>
+    </div>
+  );
+}
+
+export default App;
