65f73e57d280d066cdd5545014ffcbd31cd23f94
Laura Vikanis <lauravikanis@yahoo.de>
2021-08-15 15:36:18 +0200

    💬 Edit About text

--- src/App.tsx
+++ src/App.tsx
@@ -131,22 +131,16 @@ const App = () => {
           </Menueitem>
           <AboutDiv>
             <p>
-              Switching careers during a global pandemic is impossible? No, it
-              is not! Starting in Summer 2020 I attended a 4-week long workshop
-              at shecodes.io where I learned the basics of HTML, CSS and basic
-              Javascript. Afterwards, I wanted to challenge myself further: I
-              decided to self-teach myself coding. In autumn I attended the Web
-              Developer Bootcamp at neue Fische, which I completed in
-              December/January. A few months later I got a full-time job at a
-              tech company where I am now part of an agile development team.
-              Even if in the last couple of months my learning curve was very
-              steep and I learned a lot I do not intend on slowing down.
+              Hi there! Nice to meet you. <br/>
+              I am Laura, a Web Developer from Cologne, Germany. <br/>
+              I worked as a graphic designer for 8 years but in the end, I decided to switch careers.
+              I started my Developer career in summer 2020 by attending a 4-week workshop with shecodes.io. I got hooked immediately and decided to attend a 3month Bootcamp at neue Fische which I successfully graduated in January 2021.  
+              The most important thing I learned during this phase of my life was to never give up on my dreams and never be too shy to ask questions. There is always a solution to problems and I do not intend to reduce my steep learning course.
             </p>
             <img src={zickzackpath} alt="zickzack" />
             <p>
               Apart from working a lot on my computer, one of my main interests
-              is music. I enjoy going to concerts (hopefully again after corona)
-              and looking for new artists, tracks, and genres. Apart from music
+              is music. I enjoy going to concerts and looking for new artists, tracks, and genres. Apart from music
               I also enjoy other aspects of arts and cultur such as visual and
               performing arts.
             </p>
